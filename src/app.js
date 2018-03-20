95ab4b5777241eed71a73de2ef89ef678f2608b8
li.li <mark.li210812@gmail.com>
2018-03-20 13:32:21 +0800

    :bug: fix: lodash too large

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import preact from 'preact'
+// import preact from 'preact'
 // import _ from 'lodash'
-import './bar'
+// import './bar'
 // import './app.css'
-import(/* webpackChunkName: "async-bar" */'./async-bar').then(a => console.log(a))
-import(/* webpackChunkName: "async-foo" */'./async-foo').then(a => console.log(a))
+// import(/* webpackChunkName: "async-bar" */'./async-bar').then(a => console.log(a))
+// import(/* webpackChunkName: "async-foo" */'./async-foo').then(a => console.log(a))
 // console.log(preact.toString())
 // console.log('hello preact')
 export default function app() {
